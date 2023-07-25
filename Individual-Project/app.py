b56ce98314f39176558c418fc3e2cd3855179408
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-25 17:00:44 +0300

    hh

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -19,6 +19,41 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        full_name = request.form["full_name"]
+        user = { 'email': email,'password': password,'full_name': full_name}
+        try:
+            login_session['user']=auth.create_user_with_email_and_password(email, password)
+            user_uid = login_session['user']['localId']
+            db.child("Users").child(user_uid).set(user)
+            return redirect(url_for('personal_page'))
+        except:
+            return redirect(url_for("signup"))
+    else:
+        return render_template('signup.html')
+        
+@app.route('/personal_page', methods=["GET","POST"])
+def personal_page():
+    return render_template("personal_page.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+    if request.method=="POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user']=auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('personal_page'))
+        except:
+            return redirect(url_for("signin"))
+    else:
+        return render_template("signin.html")
+
 
 
 #Code goes above here
