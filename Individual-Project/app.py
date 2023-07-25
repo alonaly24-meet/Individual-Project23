7725e450fe129a75f4e5a10943369efb94908423
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-25 12:32:01 +0300

    ,

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -6,7 +6,18 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
+config = {
+  "apiKey": "AIzaSyBdi9Ygj_BXFuKqzBZPQWkSVGfhgiO4pGE",
+  "authDomain": "personal-project-crazy-crazy.firebaseapp.com",
+  "projectId": "personal-project-crazy-crazy",
+  "storageBucket": "personal-project-crazy-crazy.appspot.com",
+  "messagingSenderId": "793628397478",
+  "appId": "1:793628397478:web:bece2dcff34c9d05190967",
+  "measurementId": "G-8EK7YMSC4F", "databaseURL":"https://personal-project-crazy-crazy-default-rtdb.firebaseio.com/"}
 
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
 
 
 
