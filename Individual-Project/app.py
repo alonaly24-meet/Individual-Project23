1014841cf262d49cc7da0cc308de23ee4c665a27
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-26 12:33:34 +0300

    m
    mm

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -19,6 +19,10 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
+def get_checked_items(user_uid):
+    user_data = db.child("Users").child(user_uid).get()
+    return user_data.val() or {}
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -35,26 +39,75 @@ def signup():
             return redirect(url_for("signup"))
     else:
         return render_template('signup.html')
-        
-@app.route('/personal_page', methods=["GET","POST"])
-def personal_page():
-    return render_template("personal_page.html")
 
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=["GET","POST"])
 def signin():
-    if request.method=="POST":
+    if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
         try:
-            login_session['user']=auth.sign_in_with_email_and_password(email, password)
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+
+            # Retrieve the user's checklist data from the database and store it in checked_items
+            user_uid = login_session['user']['localId']
+            user_data = db.child("Users").child(user_uid).get()
+            login_session['checked_items'] = user_data.val() or {}
+
             return redirect(url_for('personal_page'))
+
         except:
+            login_session.pop('checked_items', None)
             return redirect(url_for("signin"))
     else:
         return render_template("signin.html")
 
+@app.route('/personal_page', methods=["GET", "POST"])
+def personal_page():
+    if 'user' in login_session:
+        user_uid = login_session['user']['localId']
+        checked_items = get_checked_items(user_uid)
+    else:
+        checked_items = {}
 
+    if request.method == 'POST':
+        item1 = request.form.get("item1")
+        item2 = request.form.get("item2")
+        item3 = request.form.get("item3")
+        item4 = request.form.get("item4")
+        item5 = request.form.get("item5")
+        item6 = request.form.get("item6")
+        item7 = request.form.get("item7")
+
+        if user_uid:
+            user_checklist = {
+                "item1": item1,
+                "item2": item2,
+                "item3": item3,
+                "item4": item4,
+                "item5": item5,
+                "item6": item6,
+                "item7": item7
+            }
+        db.child("Users").child(user_uid).set(user_checklist)
+
+        return redirect(url_for("survey"))
+    
+    return render_template("personal_page.html", checked_items=checked_items)
+
+@app.route("/survey",methods=["GET","POST"])
+def survey():
+    if request.method == 'POST':
+        return render_template("tips.html")
+    else:
+        return render_template("tips.html")
+
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 #Code goes above here
 
